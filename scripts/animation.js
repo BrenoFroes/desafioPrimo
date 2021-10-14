d22ab47bb204d801222ea2c39d73fa8543dbcfa2
Breno Froes <breno_froes@id.uff.br>
2021-10-14 16:45:09 -0300

    [adjust] Finalizacao das animacoes

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,12 +1,13 @@
-var body = document.querySelector("body");
-var text = document.getElementsByClassName('green-background-text');
-
-for (let i = 0; i < text.length; i++){ 
-    document.addEventListener("scroll", typeWrite(text[i]));
+function textAnimate(body) {
+    body.className += " animate";
 }
 
-function textAnimate() {
-    body.className += " animate";
+function imgAnimate(crease){
+    let img = document.getElementsByClassName("img-eigth-section")[0];
+    if(crease)
+        img.style.maxWidth = '300px';
+    else
+        img.style.maxWidth = '150px';
 }
 
 function typeWrite(element){
@@ -18,5 +19,11 @@ function typeWrite(element){
 }
 
 window.onload = function() {
-    textAnimate();
+    var body = document.querySelector("body");
+    var text = document.getElementsByClassName('green-background-text');
+    
+    textAnimate(body);
+    for (let i = 0; i < text.length; i++){ 
+        document.addEventListener("scroll", typeWrite(text[i]));
+    }
 };
